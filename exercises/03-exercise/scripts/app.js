991db8d5640a9489988a884af0cc7fdfc6251104
Janet-Ifeoma-Ogbu <io67@drexel.edu>
2026-01-26 22:39:45 -0500

    assignment 3

---
+++ exercises/03-exercise/scripts/app.js
@@ -0,0 +1,38 @@
+//Calculate 
+function greetPerson(name) {
+  console.log(`Hello, ${name}! Welcome!`);
+}
+
+greetPerson('Maame');
+greetPerson('Dami');
+greetPerson('Ifeoma');
+
+
+//Calculate Area
+
+function calculateArea(width, height) {
+  const area = width * height;
+  console.log(`Area: ${area} square units`);
+}
+
+calculateArea(5, 10);  
+calculateArea(8, 12);
+calculateArea(3, 7);  
+
+
+
+
+
+const greetPersonArrow = (name) => {
+  console.log(`Hello, ${name}!`) ;
+};
+
+greetPersonArrow('Tolu');
+
+
+const calculateAreaArrow  = (width, height) => {
+    const area = width * height    
+    console.log(`Area: ${area} square units`);
+};
+
+calculateArea(30,30)
